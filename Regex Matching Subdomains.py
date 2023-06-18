ddc0b80334db1f5e84603ac4051f2b44d7bad70f
Akrem Basha <akrem.basha@gmail.com>
2023-06-17 23:11:02 -0500

    Update Regex Matching Subdomains.py

--- Regex Matching Subdomains.py
+++ Regex Matching Subdomains.py
@@ -4,6 +4,6 @@ subdomains = ['www.microsoft.com/9023', 'office.microsoft.com', 'skype.msft.com/
 msftsubdomains = []
 
 for i in subdomains:
-    if re.search('(?<=\.)microsoft.com', i) != None:
+    if re.search('(?<=\.)microsoft.com/', i) != None:
         msftsubdomains.append(i)
 print(msftsubdomains)
